5a2e13ad619efe755029168a8da9994ffaeb1417
mvipe <mohnishvats@gmail.com>
2022-05-21 03:26:28 +0530

    Adding The Profile Image

--- app/src/main/java/com/learn/bizcard/MainActivity.kt
+++ app/src/main/java/com/learn/bizcard/MainActivity.kt
@@ -3,7 +3,10 @@ package com.learn.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -13,6 +16,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.learn.bizcard.ui.theme.BizCardTheme
@@ -39,12 +45,25 @@ fun createBizCard(){
     Surface( modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
-        Card(modifier = Modifier.width(200.dp)
-            .height(390.dp).padding(20.dp)
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(20.dp)
             , shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             backgroundColor = Color.White
             , elevation = 4.dp) {
-
+                Surface(modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp)
+                    , shape = CircleShape,
+                    border = BorderStroke(0.5.dp, Color.LightGray),
+                    elevation = 4.dp,
+                    color = MaterialTheme.colors.onSurface.copy(alpha=0.5f)
+                ) {
+                    Image(painter = painterResource(id = R.drawable.user), contentDescription ="Profile Image"
+                    , modifier = Modifier.size(135.dp)
+                    , contentScale = ContentScale.Crop)
+                }
         }
     }
 }
